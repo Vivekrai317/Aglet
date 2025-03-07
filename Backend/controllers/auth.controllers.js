e0dec31fba165f8b2597d6d9bc60a552e5b86848
Vivekrai317 <vivekrai317@gmail.com>
2025-03-07 11:26:25 +0530

    auth controllers

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,5 +1,38 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js"
+import jwt from "jsonwebtoken";
 
+const generateTokens = (userId)=>{
+    const accessToken = jwt.sign({userId},process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:"15m",
+    })
+
+    const refreshToken = jwt.sign({userId},process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn:"7d",
+    })
+
+    return {accessToken,refreshToken}
+}
+
+const storeRefreshToken = async(userId,refreshToken)=>{
+    await redis.set(`refresh_token:${userId}`,refreshToken,"EX",7*24*60*60)
+}
+
+
+const setCookies = (res,accessToken,refreshToken)=>{
+    res.cookie("accessToken",accessToken,{
+        httpOnly:true, //prevents xss attacks
+        secure:process.env.NODE_ENV ==="production",
+        sameSite:"strict", //prevents CSRF attack, cross-site request forgery
+        maxAge:15*60*1000
+    })
+    res.cookie("refreshToken",refreshToken,{
+        httpOnly:true, //prevents xss attacks
+        secure:process.env.NODE_ENV ==="production",
+        sameSite:"strict", //prevents CSRF attack, cross-site request forgery
+        maxAge:7*24*60*60*1000
+    })
+}
 export const signup = async(req,res)=>{
     const {email,password,name} = req.body;
     try {
@@ -9,17 +42,64 @@ export const signup = async(req,res)=>{
         }
     
         const user = await User.create({name,email,password})
-    
-        res.status(201).json({message:"User created successfully"})
+
+        // authenticate user using jsonwebtoken and redis 
+        const {accessToken, refreshToken} =  generateTokens(user._id)
+        await storeRefreshToken(user._id,refreshToken)
+
+        setCookies(res,accessToken,refreshToken);
+        res.status(201).json({
+            user:{
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role,
+            }
+        ,message:"User created successfully"})
     } catch (error) {
+        console.log("Error in singup controller",error.message);
         res.status(500).json({message:error.message})
     }
 }
 
 export const login = async(req,res)=>{
-
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email})
+        if(user && (await user.comparePassword(password))){
+            const {accessToken,refreshToken} = generateTokens(user._id)
+            await storeRefreshToken(user._id,refreshToken)
+            setCookies(res,accessToken,refreshToken)
+            res.json({
+                _id:user.id,
+                name:user.name,
+                email:user.email,
+                role:user.role
+            })
+        }else{
+            res.status(401).json({message:"Invalid email or Password"})
+        }
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        res.status(500).json({
+            message:error.message
+        })
+    }
 }
 
 export const logout = async(req,res)=>{
-    
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if(refreshToken){
+            const decoded = jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET)
+            await redis.del(`refresh_token:${decoded.userId}`)
+        }
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken")
+        res.json({message:"Logged out successfully"})
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message:"Internal server error",error:error.message})
+    }
 }
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
 
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()) // allows to parse body of request
+app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
