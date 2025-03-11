7869ea400344a27dc6ce23d97002a4ca2b30d6a2
Vivekrai317 <vivekrai317@gmail.com>
2025-03-11 16:48:23 +0530

    coupon codes

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import { connectDB } from "./lib/db.js";
 import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/products",productRoutes)
 app.use("/api/cart",cartRoutes)
+app.use("/api/coupons",couponRoutes)
 
 app.listen(PORT,()=>{
     console.log("Server running on port",PORT);
