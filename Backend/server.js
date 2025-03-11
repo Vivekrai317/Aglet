af6a0bab2d765312d6c41a021fd585a78f30b73c
Vivekrai317 <vivekrai317@gmail.com>
2025-03-11 16:28:35 +0530

    cart controllers

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import { connectDB } from "./lib/db.js";
-
+import cartRoutes from "./routes/cart.route.js"
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -17,6 +17,7 @@ app.use(express.json()) // allows to parse body of request
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/products",productRoutes)
+app.use("/api/cart",cartRoutes)
 
 app.listen(PORT,()=>{
     console.log("Server running on port",PORT);
